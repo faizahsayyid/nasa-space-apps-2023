83e9cd7dfe35983e6551cce9be555198efe4159a
faizahsayyid <faizah.sayyid@gmail.com>
2023-10-07 16:09:57 -0400

    feat: search and create on projects

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
 var cors = require("cors");
 const express = require("express");
+const projectRoutes = require("./routes/projects");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use("/project", projectRoutes);
 
 app.get("/", (req, res) => {
   res.send({ hello: "world" });
