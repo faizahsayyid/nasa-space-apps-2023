3a56cb926c9cc005b37d5ff8bf268290b48b5121
faizahsayyid <faizah.sayyid@gmail.com>
2023-10-08 17:43:10 -0400

    fix: refactor header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
-import { ChakraProvider } from '@chakra-ui/react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Home, Search } from './pages';
+import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { Home, Search } from "./pages";
+import Navbar from "./components/Navbar";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/search',
-    element: <Search />,
+    path: "/",
+    element: (
+      <div>
+        <Navbar />
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+    ],
   },
 ]);
 
